6e146817bab35db0e58fb34fb99152982c8e5660
BT_1N3_30 <cpolisi6526@bths.edu>
2018-12-03 08:40:20 -0500

    1

--- src/Ship.java
+++ src/Ship.java
@@ -1,2 +1,10 @@
 public class Ship {
+    String name;
+    private int xLoc;
+    private int yLoc;
+    public Ship(String name, int xLoc, int yLoc){
+        this.name=name;
+        this.xLoc= xLoc;
+        this.yLoc= yLoc;
+    }
 }
